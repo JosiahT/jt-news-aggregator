53df6ed707dfd8b11973627e05b8a56d572e79c1
Josiah <38712734+JosiahT@users.noreply.github.com>
2022-11-06 21:51:47 -0800

    Added Contract project Added response contract

---
+++ src/NewsAggregator.Contracts/Response/NewsResponse.cs
@@ -0,0 +1,18 @@
+﻿using System.Security;
+
+namespace NewsAggregator.Contracts.Response;
+
+public class NewsResponse
+{
+    public Guid Id { get; set; }
+    public string? Title { get; set; }
+    public string? Synopsis { get; set; }
+    public string? Link { get; set; }
+    public NewsDetail? Detail {get; set;}
+    public string? Thumbnail { get; set; }
+    public IEnumerable<string>? Tags { get; set; }
+    public string Category { get; set; }
+    public DateTime? DatePublished { get; set; }
+    public Author? Author { get; set; }
+    public Source? Source { get; set; }
+}
--- tests/NewsAggregator.Web.UnitTests/UnitTest1.cs
+++ tests/NewsAggregator.Web.UnitTests/UnitTest1.cs
@@ -3,8 +3,5 @@ namespace NewsAggregator.Web.UnitTests;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
-    {
-
-    }
+    public void Test1() { }
 }
